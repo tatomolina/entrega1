6e2f1c42d961ce7e41f333e2dbcb0824ad4275ac
Tato <gastomolina91@gmail.com>
2015-04-03 02:22:42 -0300

    Agrego la logica para que la aplicacion se siga reproduciendo hasta q se le dice salir a esta

--- cmd.rb
+++ cmd.rb
@@ -1,17 +1,23 @@
 require 'highline/import'
 
-choose do |menu|
-	menu.header = "Menu"
-	menu.prompt = "Elija una opcion: "
-	menu.choice(:Login) do
-		say "Logueando..."
-	end
-	menu.choice(:Logout) do
-		say "Deslogueando..."
-	end
-	menu.choice(:Estado) do
-		say "Tu estado es"
-	end
-	menu.choice(:Salir) do
+salir = false;
+
+while !salir do
+
+	choose do |menu|
+		menu.header = "Menu"
+		menu.prompt = "Elija una opcion: "
+		menu.choice(:Login) do
+			say "Logueando..."
+		end
+		menu.choice(:Logout) do
+			say "Deslogueando..."
+		end
+		menu.choice(:Estado) do
+			say "Tu estado es"
+		end
+		menu.choice(:Salir) do
+			salir = true		
+		end
 	end
 end
